3c722681e89d815387690c168ffdf9d854772265
AimJim <92112507+AimJim@users.noreply.github.com>
2023-04-28 19:26:58 +0200

    user created

---
+++ Server/src/data/User.java
@@ -0,0 +1,18 @@
+package data;
+
+import java.util.ArrayList;
+
+public class User {
+
+	private String username;
+	private String password;
+	private ArrayList<Playlist> playlist = new ArrayList<Playlist>();
+	private boolean admin = false;
+	
+	
+	//TODO
+	
+	public void addSong(Song song) {
+		
+	}
+}
